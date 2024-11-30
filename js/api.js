50ba42e31632c9f0fab9aadd4d889486c5792f0f
valeria cilento <valeecilento@gmail.com>
2024-11-29 23:55:13 -0300

    Se agrega catch de fetch

--- js/api.js
+++ js/api.js
@@ -2,7 +2,7 @@
 const geoSuccess = (pos) => {
    let lat = pos.coords.latitude
    let lon = pos.coords.longitude
-   const apiOpenWeather = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=00f9c6f5fc71e26725f9c229f684239f&units=metric`
+   const apiOpenWeather = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=570b464168922cfe5ecbd90b16266f82&units=metric`
    fetch(apiOpenWeather)
       .then(response => response.json())
       .then(data => {
@@ -12,8 +12,10 @@ const geoSuccess = (pos) => {
                                     <p>Temperatura: ${Math.round(data.main.temp)}°C</p>
                                     <p>Sensación térmica: ${Math.round(data.main.feels_like)}°C</p>`
          climaDiv.append(climaDivInfo)
-      }) 
-
+      })
+      .catch(error=>{
+         throw(error)
+      })
 }
 
 const geoError = () => {
